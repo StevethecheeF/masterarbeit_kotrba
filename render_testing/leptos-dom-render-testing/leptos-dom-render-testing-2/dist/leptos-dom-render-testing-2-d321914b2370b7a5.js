a4adcdbeba5123dfb5662147b21161b921909ea0
Stefan Kotrba <stefan.kotrba@student.uibk.ac.at>
2024-09-14 09:11:18 +0200

    cleanup

--- render_testing/leptos-dom-render-testing/leptos-dom-render-testing-2/dist/leptos-dom-render-testing-2-d321914b2370b7a5.js
+++ render_testing/leptos-dom-render-testing/leptos-dom-render-testing-2/dist/leptos-dom-render-testing-2-d321914b2370b7a5.js
@@ -831,7 +831,7 @@ async function __wbg_init(module_or_path) {
     console.warn('using deprecated parameters for the initialization function; pass a single object instead')
 
     if (typeof module_or_path === 'undefined') {
-        module_or_path = new URL('leptos-dom-render-testing-2-d321914b2370b7a5_bg.wasm', import.meta.url);
+        module_or_path = new URL('leptos-dom-render-testing-2_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
