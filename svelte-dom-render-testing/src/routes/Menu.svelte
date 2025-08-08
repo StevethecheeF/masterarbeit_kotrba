e08b7501c078c509b7bb0ea95aab40dee71da252
StevethecheeF <kotrba.stefan@gmx.com>
2025-08-08 09:58:45 +0200

    finalize svelte project

--- svelte-dom-render-testing/src/routes/Menu.svelte
+++ svelte-dom-render-testing/src/routes/Menu.svelte
@@ -1,5 +1,5 @@
 <script>
-    export let numberOfRows = 0;
+	export let numberOfRows = 0;
 	export let createRows;
 	export let addRows;
 	export let removeRows;
@@ -7,29 +7,31 @@
 </script>
 <div>
     <div>
-        <h1>Framework: Svelte</h1>
-    </div>
-    <div>
-        <p>total number of rows: {numberOfRows} </p>
-    </div>
-    <div>
-        <button id="create-1000" on:click={createRows(1000)}>
-            create 1000
-        </button>
-        <button id="add-1000" on:click={addRows(1000)}>
-            add 1000
-        </button>
-        <button id="create-10000" on:click={createRows(10000)}>
-            create 10000
-        </button>
-        <button id="add-10000" on:click={addRows(10000)}>
-            add 10000
-        </button>
-        <button id="update" on:click={updateRows}>
-            update all
-        </button>
-        <button id="remove" on:click={removeRows}>
-            remove all
-        </button>
+        <div id="framework">
+            <h1>Framework: Svelte</h1>
+        </div>
+        <div>
+            <p>total number of rows: {numberOfRows} </p>
+        </div>
+        <div>
+            <button id="create-1000" on:click={createRows(1000)}>
+                create 1000
+            </button>
+            <button id="add-1000" on:click={addRows(1000)}>
+                add 1000
+            </button>
+            <button id="create-10000" on:click={createRows(10000)}>
+                create 10000
+            </button>
+            <button id="add-10000" on:click={addRows(10000)}>
+                add 10000
+            </button>
+            <button id="update" on:click={updateRows}>
+                update all
+            </button>
+            <button id="remove" on:click={removeRows}>
+                remove all
+            </button>
+        </div>
     </div>
 </div>
